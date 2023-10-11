063f16af94be2b8022bfc6f84b5ab22e56bda8c9
A3DevUser <A3versioncontrol@gmail.com>
2023-10-11 17:57:16 +0530

    nitin elements pop up and section

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import React, { useEffect, useState } from 'react'
-import { MainObject } from './commonFun'
-import axios from 'axios';
+import { useState } from 'react';
+import './App.css';
+import { MainObject } from './Component/commonFun';
+// import { Button } from 'react-bootstrap';
+// import Modalpop from './Component/Modalpop';
+// import SampleComp from './SampleComp';
+import Table from './Component/Table';
+// import { MainObject } from './Component/commonFun';
+// import Table from './Component/Table';
+// import { MainObject } from './Component/commonFun';
 
-const App = () => {
-    const [btnVal, setbtnVal] = useState()
-    const funaction = () => {return "Button Clicked"};
-    useEffect(() => {
-        axios.get(`http://localhost:8080/VF/getBtn?formId=Form-101`)
-        .then((res) => {setbtnVal(res.data)})
-    },[])
-  return (
-    <>
-    {
-        btnVal.map((res) => {
-            return <>{MainObject.button(res.classNameVal,res.widthVal.res.heightVal,res.btnName,funaction)}</>
-        })
-        
-    }
-    </>
-  )
+function App() {
+  let obj = [{name: "Audit",value: "Pass"},
+             {name: "Audit",value: "Fail"},
+             {name: "Audit",value: "NA"}]
+
+  // let accordionobj = [{secid: "sec01",sechead: "First Section", secbody: "Hello Word...!!",isOpen : true},
+  // {secid: "sec02",sechead: "Second Section", secbody: "Hello New Word...!!", isOpen : false}]
+
+  const [modalShow, setModalShow] = useState(false);
+  const funaction = () => setModalShow(true);
+             return (
+    <div className="App">
+      {/* <button onClick={()=>{MainObject.alert("Hiii...!")}}>Say Hi</button>
+      {
+        MainObject.button("m-3","Submit","100px","30px")
+      }
+      {
+        MainObject.input("m-3","text","100px","30px")
+      }
+
+      {
+        MainObject.accordion(accordionobj)
+      }
+      {
+        MainObject.loader()
+      } */}
+      
+      {/* <Button variant="primary" onClick={() => setModalShow(true)}>Save</Button> */}
+
+      {
+        MainObject.button("btn btn-primary btn-sm","90px","30px","Save",funaction)
+      }
+      {
+        MainObject.modalpop(modalShow, setModalShow,<Table/>,"Modal heading1")
+      }
+            {
+        MainObject.dropdown(obj)
+      }
+
+      {/* <Modalpop show={modalShow} setModalShow={setModalShow} bodyDetails={<Table/>} titleDetails="Modal heading1"/> */}
+    
+    </div>
+  );
 }
 
-export default App
+export default App;

1414d2d6cd99862622401413c35cc14084b04ce4
A3DevUser <a3versioncontrol@gmail.com>
2023-10-12 19:39:13 +0530

    commit

--- src/Component/Form.jsx
+++ src/Component/Form.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { FetchSectionData } from '../Store/Actions/Section';
 import { MainObject } from './Elements/commonFun';
-// import FormTable from './FormTableDir/FormTable';
+import FormTable from './FormTableDir/FormTable';
 import { FetchColumnData } from '../Store/Actions/Column';
 
 const Form = () => {
@@ -13,21 +13,22 @@ const Form = () => {
 
     useEffect(()=>{
         dispatch(FetchSectionData('Form-101'))
+        dispatch(FetchColumnData('Form-101'))
     },[])
 
     useEffect(()=>{
-        dispatch(FetchColumnData('Form-101'))
-    },[])
+      console.log(SectionRed)
+      console.log(ColumnRed)
+    },[SectionRed,ColumnRed])
 
     const colData = [{fieldName : 'Checker', accessor : 'checker',secId : '001'},{fieldName : 'Maker', accessor : 'maker',secId : '001'}]
 
   return (
     <div>
-      {
-        SectionRed.loading ? MainObject.loader() : 
-        ColumnRed.loading ? MainObject.loader() : 
-        MainObject.accordion(SectionRed.val,ColumnRed.val,[])
-      }
+      {/* {
+        SectionRed.loading ? MainObject.loader() : ColumnRed.loading ? MainObject.loader() : 
+        MainObject.accordion(SectionRed.val,ColumnRed.val,[]) 
+      } */}
     </div>
   )
 }

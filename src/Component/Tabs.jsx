b4be28af1d767f458f58dc349d4a4fae9271e3ce
A3DevUser <A3versioncontrol@gmail.com>
2023-10-19 18:43:40 +0530

    Section NavBar added

--- src/Component/Tabs.jsx
+++ src/Component/Tabs.jsx
@@ -1,8 +1,9 @@
 import { Tab, Tabs } from "react-bootstrap"
 import { MainObject } from "./Elements/commonFun"
 
-function TabsBar({accordionVal,gridData,columnData,data}) {
-    return   <Tabs defaultActiveKey="profile" id="fill-tab-example" className="mb-3" fill>
+function TabsBar({accordionVal,gridData,columnData,data,defaultVal}) {
+    console.log('defaultVal',defaultVal)
+    return   <Tabs activeKey={defaultVal[0]} id="fill-tab-example" className="mb-3" fill>
         {
             accordionVal.map((res,i)=>{
                 return <Tab eventKey={res.secId} title={res.secName}>
@@ -10,7 +11,7 @@ function TabsBar({accordionVal,gridData,columnData,data}) {
                 gridData.filter((fil)=>{
                     return fil.secId == res.secId
                 }).map((subRes)=>{
-                    return (<><br/><h3 eventKey={subRes.subSecId}>{subRes.subSecName}</h3>
+                    return (<><br/><h3 id={subRes.subSecId}>{subRes.subSecName}</h3>
                     <div style={{maxWidth : subRes.width, overflow:'scroll', maxHeight:'50vh'}}>
                         {
                         subRes.subSecType == 'grid' ?

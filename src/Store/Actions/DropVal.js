30ad9830e5551c3c64e4f9e8cd958bf72569e460
A3DevUser <A3versioncontrol@gmail.com>
2023-11-08 13:54:20 +0530

    drop down files updated

--- src/Store/Actions/DropVal.js
+++ src/Store/Actions/DropVal.js
@@ -21,10 +21,11 @@ const DropValErr = (val) =>{
     }
 };
 
-export const FetchDropValData = (FormId,GridId,ColId) =>{
+export const FetchDropValData = (FormId,GridId,ColId,JSON) =>{
+    console.log("JSONval",JSON)
     return (dispatch)=>{
         dispatch(DropValReq());
-        axios.get(`http://localhost:8080/VF/dropdown?formId=${FormId}&colId=${ColId}&gridId=${GridId}`)
+        axios.get(`http://localhost:8080/VF/dropdown?formId=${FormId}&colId=${ColId}&gridId=${GridId}&jsonDrop=${JSON}`)
         .then((res)=>{
             dispatch(DropValSuccess(res.data))
         })
